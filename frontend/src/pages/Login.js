81b0dee7e6307c1c11fcce75f45333176ed2191c
Yash Gadodia <82965913+yashgadodia12@users.noreply.github.com>
2025-03-14 01:48:00 +0000

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 const Login = () => {
     const [email, setEmail] = useState("");
